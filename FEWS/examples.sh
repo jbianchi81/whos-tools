ba4e7b329953ee130602b0fb8aa86e8a1feabd86
jbianchi81 <84208498+jbianchi81@users.noreply.github.com>
2022-05-03 15:13:04 -0300

    Update examples.sh

--- FEWS/examples.sh
+++ FEWS/examples.sh
@@ -7,4 +7,4 @@ python3 whos_client.py monitoringPoints --json results/mp.json --fews results/mp
 # get timeseries by station ID 
 python3 whos_client.py timeseries --monitoringPoint 0009BBB009E7F4067B498FC0073C2AA63D064D27 --json results/ts.json --fews results/ts.csv
 # download and convert all stations and time series metadata as required by FEWS
-python3 whos_client.py all
+python3 whos_client.py all -O results
