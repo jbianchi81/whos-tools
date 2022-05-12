0f6857eb539041991b897c40484f950d2d92ed18
jbianchi81 <84208498+jbianchi81@users.noreply.github.com>
2022-05-12 16:44:49 -0300

    Add files via upload

---
+++ FEWS/a5ToFews.py
@@ -0,0 +1,179 @@
+from typing import Union
+from pathlib import Path
+from geopandas import read_file as gpd_read_file
+from shapely.geometry import Point
+import json
+import pandas
+from warnings import warn
+
+config = {
+    "basins_geojson_file": "cuencas/cuencas.geojson"
+}
+
+basins = gpd_read_file(config["basins_geojson_file"])
+
+def interval2epoch(interval):
+    seconds = 0
+    for k in interval:
+        if k == "milliseconds" or k == "millisecond":
+            seconds = seconds + interval[k] * 0.001
+        elif k == "seconds" or k == "second":
+            seconds = seconds + interval[k]
+        elif k == "minutes" or k == "minute":
+            seconds = seconds + interval[k] * 60
+        elif k == "hours" or k == "hour":
+            seconds = seconds + interval[k] * 3600
+        elif k == "days" or k == "day":
+            seconds = seconds + interval[k] * 86400
+        elif k == "weeks" or k == "week":
+            seconds = seconds + interval[k] * 86400 * 7
+        elif k == "months" or k == "month" or k == "mon":
+            seconds = seconds + interval[k] * 86400 * 31
+        elif k == "years" or k == "year":
+            seconds = seconds + interval[k] * 86400 * 365
+    return seconds
+
+def estacionesToFews(estaciones : Union[str, list],output=None): 
+    """Converts a5 estaciones to FEWS table
+    
+    Parameters
+    ----------
+    estaciones : dict or str
+        Result of a5_client.getEstaciones
+        If str: JSON file to read from
+        If list: a5_client.getEstaciones 
+    output: string
+        Write CSV output into this file
+    
+    Returns
+    -------
+    DataFrame
+        A data frame of the stations in FEWS format
+    """
+    if isinstance(estaciones,str):
+        with open(estaciones,"r") as f: 
+            estaciones = json.load(f)
+    rows = []
+    for item in estaciones:
+        if not isinstance(item["geom"]["coordinates"][0],float):
+            warn("Station %i with no geometry. Skipping" % item["id"])
+            continue
+        row = {
+            "STATION_ID": item["id"],
+            "STATION_NAME": item["nombre"],
+            "STATION_SHORTNAME": item["abreviatura"] if item["abreviatura"] is not None else item["nombre"].replace(" ","")[0:12],
+            "TOOLTIP": "station %i: %s%s" % (item["id"], item["nombre"], (" - property of %s" % item["propietario"]) if item["propietario"] is not None else ""),
+            "LATITUDE": item["geom"]["coordinates"][1],
+            "LONGITUDE": item["geom"]["coordinates"][0],
+            "ALTITUDE": item["altitud"],
+            "COUNTRY": item["pais"],
+            "ORGANIZATION": "INA",
+            "SUBBASIN": getSubBasin(item["geom"]["coordinates"])
+        }
+        rows.append(row)
+    data_frame = pandas.DataFrame(rows).sort_values("STATION_ID")
+    if output is not None:
+        try: 
+            f = open(output,"w")
+        except:
+            raise Exception("Couldn't open file %s for writing" % output)
+        f.write(data_frame.to_csv(index=False))
+        f.close()
+    return data_frame
+
+
+def getSubBasin(coordinates):
+    # def get_region(lon,lat,getNotation=False):
+    st0 = Point(coordinates[0],coordinates[1])
+    subbasin = None
+    for i in range(0,len(basins.geometry)):
+        if st0.within(basins.geometry[i]):
+            # print("is within subbasin %s" % self.basins.nombre_2[i])
+            subbasin = basins.nombre_2[i]
+    return subbasin
+
+def seriesToFews(series : Union[str,list], output=None):
+    """Converts a5 series list to FEWS table
+    
+    Parameters
+    ----------
+    series : dict or list
+        Result of a5_client.getSeries
+        If str: JSON file to read from
+        If dict: a5_client.getSeries return value  
+    output: string
+        Write CSV output into this file
+    
+    Returns
+    -------
+    DataFrame
+        A data frame of the time series in FEWS format
+    """
+    
+    # timeseries: str => path to timeseries geojson file or dict => same already parsed into dict 
+    if isinstance(series,str):
+        with open(series,"r") as f: 
+            series = json.load(f)
+    rows = []
+    for item in series:
+        row = {
+            "STATION_ID": item["estacion"]["id"],
+            "EXTERNAL_LOCATION_ID": item["estacion"]["id"],
+            "EXTERNAL_PARAMETER_ID": item["var"]["id"],
+            "TIMESTEP_HOUR": interval2epoch(item["var"]["timeSupport"]) / 3600 if item["var"]["timeSupport"] is not None and len(item["var"]["timeSupport"].keys()) else None,
+            "UNIT": item["unidades"]["abrev"],
+            "IMPORT_SOURCE": "INA",
+            "THRESHOLD_LOW_WATERS": item["estacion"]["nivel_aguas_bajas"] if item["var"]["VariableName"] == "Gage height" else None,
+            "THRESHOLD_FLOOD_ALERT": item["estacion"]["nivel_alerta"] if item["var"]["VariableName"] == "Gage height" else None,
+            "THRESHOLD_EVACUATION": item["estacion"]["nivel_evacuacion"] if item["var"]["VariableName"] == "Gage height" else None
+        }
+        rows.append(row)
+    data_frame = pandas.DataFrame(rows).sort_values(["STATION_ID","EXTERNAL_PARAMETER_ID"])
+    if output is not None:
+        try: 
+            f = open(output,"w")
+        except:
+            raise Exception("Couldn't open file %s for writing" % output)
+        f.write(data_frame.to_csv(index=False))
+        f.close()
+    return data_frame
+
+
+if __name__ == "__main__":
+    from a5_client import Client
+    a5_client = Client()
+    estaciones = a5_client.getEstaciones(has_obs=True, pais="Argentina", habilitar=True)
+    # len(estaciones)
+    # estaciones_fews = estacionesToFews("results/estaciones.json",output="results/estaciones_fews.csv")
+    estaciones_fews = estacionesToFews(estaciones,output="results/estaciones_fews.csv")
+    # SERIES
+    estacion_ids = [id for id in estaciones_fews["STATION_ID"]]
+    series = []
+    i = 0
+    by = 10
+    while i < len(estacion_ids):
+        series_part = a5_client.getSeries(proc_id=[1,2],var_id=[1,2,4,39,40],estacion_id=estacion_ids[i:i+by])
+        series.extend(series_part)
+        i = i + by
+    #len(series)
+    #set([s["procedimiento"]["id"] for s in series])
+    #set([s["estacion"]["id"] for s in series])
+    # FILTER OUT by timeend
+    from datetime import datetime, timedelta
+    how_old_days = 180
+    series_filter = filter(lambda serie: serie["date_range"]["timeend"] is not None and datetime.fromisoformat(serie["date_range"]["timeend"].replace("Z","")) > datetime.now() - timedelta(days=how_old_days),series)
+    series = list(series_filter)
+    series_fews = seriesToFews(series,output="results/series_fews.csv")
+    # VARIABLES
+    variables = a5_client.getVariables(id=[1,2,4,39,40],as_DataFrame=True)
+    a5_client.writeLastResult("results/variables.csv")
+    # WRITE SERIES IN SEPARATE FILES
+    for i in variables.index:
+        series_filter_by_var_id = filter(lambda serie: serie["var"]["id"] == variables["id"][i],series)
+        series_subset = list(series_filter_by_var_id)
+        filename = "results/INA_%s.csv" % variables["nombre"][i]
+        series_subset_fews = seriesToFews(series_subset,output=filename)
+    
+
+
+
---
+++ FEWS/a5_client.py
@@ -0,0 +1,196 @@
+import requests
+import pandas as pd
+import json
+from typing import Union
+
+class Client:
+    """Functions to retrieve metadata and data from a5 JSON API"""
+    
+    default_config = {
+        "url":"https://alerta.ina.gob.ar/a5",
+        "authenticate": False,
+        "token": ""
+    }
+    
+    last_result = None
+
+    def __init__(self,url : str = None, authenticate : bool = False, token : str = ""):
+        """
+        Parameters
+        ----------
+            url : str
+            authenticate : bool
+            token : str
+        """
+        
+        self.config = self.default_config
+        if url is not None:
+            self.config["url"] = url
+        if authenticate is not None:
+            self.config["authenticate"] = authenticate
+        if token is not None:
+            self.config["token"] = token
+
+    
+    def writeLastResult(self,output : str):
+        f = open(output, "w")
+        if isinstance(self.last_result,pd.DataFrame):
+            f.write(self.last_result.to_csv(index=False)) #,{"encoding":"utf-8"})
+        else:
+            f.write(json.dumps(self.last_result, ensure_ascii=False, indent=2))
+        f.close()
+    
+    def getSeries(self,tipo : str="puntual",id : Union[int,list]=None,area_id : Union[int,list]=None,estacion_id : Union[int,list]=None,escena_id : Union[int,list]=None,var_id : Union[int,list]=None,proc_id : Union[int,list]=None,unit_id : Union[int,list]=None,fuentes_id : Union[int,list]=None,tabla : Union[str,list]=None,id_externo : Union[str,list]=None,geom : str=None,include_geom : bool=None,no_metadata : bool=None, as_DataFrame : bool=False):
+        params = {"id": id,"area_id": area_id,"estacion_id": estacion_id,"escena_id": escena_id,"var_id": var_id,"proc_id": proc_id,"unit_id": unit_id,"fuentes_id": fuentes_id,"tabla": tabla,"id_externo": id_externo,"geom": geom,"include_geom": include_geom,"no_metadata": no_metadata}
+        for param in ["id","area_id","estacion_id","escena_id","var_id","proc_id","unit_id","fuentes_id","tabla","id_externo"]:
+            if isinstance(params[param],list):
+                # print("%s: %s" % (param,str(params[param])))
+                params[param] = ",".join([str(i) for i in params[param]])
+                # print("%s: %s" % (param,str(params[param])))
+        headers = {}
+        if self.config["authenticate"]:
+            headers["Authorization"] = "Bearer %s" % self.config["token"]
+        response = requests.get(
+            "%s/obs/%s/series" % (self.config["url"], tipo),
+            params = params,
+            headers = headers
+        )
+        # print("status_code: %s" % response.status_code)
+        # print("url: %s" % response.url)
+        if response.status_code > 299:
+            raise Exception(response.text)
+        json_response = response.json()
+        series = None
+        if as_DataFrame:
+            series = pd.DataFrame.from_dict(json_response)
+            if tipo == "puntual":
+                if no_metadata:
+                    series["longitude"] = [geom["coordinates"][0] for geom in series["geom"]]
+                    series["latitude"] = [geom["coordinates"][1] for geom in series["geom"]]
+                    del series["geom"]
+                else:
+                    series["longitude"] = [estacion["geom"]["coordinates"][0] for estacion in series["estacion"]]
+                    series["latitude"] = [estacion["geom"]["coordinates"][1] for estacion in series["estacion"]]
+            series["timestart"] = [date_range["timestart"] for date_range in series["date_range"]]
+            series["timeend"] = [date_range["timeend"] for date_range in series["date_range"]]
+            series["count"] = [date_range["count"] for date_range in series["date_range"]]
+            del series["date_range"]
+        else:
+            series = json_response
+        self.last_result = series
+        return series
+    
+    def getObs(self, series_id : int,timestart : str,timeend : str, tipo : str="puntual", as_DataFrame : bool=False):
+        headers = {}
+        if self.config["authenticate"]:
+            headers["Authorization"] = "Bearer %s" % self.config["token"]
+        response = requests.get(
+            '%s/obs/%s/observaciones' % (self.config["url"], tipo),
+            params={
+                'series_id': series_id,
+                'timestart': timestart,
+                'timeend': timeend
+            },
+            headers=headers
+        )
+        if response.status_code > 299:
+            raise Exception(response.text)
+        json_response = response.json()
+        obs = None
+        if as_DataFrame:
+            df_obs = pd.DataFrame.from_dict(json_response)
+            df_obs['timestart'] = pd.to_datetime(df_obs['timestart']).dt.round('min')            # Fecha a formato fecha -- CAMBIADO PARA QUE CORRA EN PYTHON 3.5
+            df_obs['timeend'] = pd.to_datetime(df_obs['timeend']).dt.round('min')            # Fecha a formato fecha -- CAMBIADO PARA QUE CORRA EN PYTHON 3.5
+            df_obs['valor'] = df_obs['valor'].astype(float)
+            # df_obs.set_index(df_obs['fecha'], inplace=True)
+            # del df_obs['fecha']
+            obs = df_obs
+        else:
+            obs = json_response
+        self.last_result = obs
+        return obs
+    
+    def getVariables(self, id : Union[int,list] = None, var : Union[str,list] = None, nombre : Union[str,list] = None, abrev : Union[str,list] = None, type : Union[str,list] = None, dataType : Union[str,list] = None, valueType : Union[str,list] = None, GeneralCategory : Union[str,list] = None, VariableName : Union[str,list] = None, SampleMedium : Union[str,list] = None, def_unit_id : Union[int,list] = None, timeSupport : Union[str,list] = None, as_DataFrame : bool=False):
+        params = {"id" : id, "var" : var, "nombre" : nombre, "abrev" : abrev, "type" : type, "dataType" : dataType, "valueType" : valueType, "GeneralCategory" : GeneralCategory, "VariableName" : VariableName, "SampleMedium" : SampleMedium, "def_unit_id" : def_unit_id, "timeSupport" : timeSupport}
+        for param in ["id", "var", "nombre" , "abrev" , "type", "dataType", "valueType", "GeneralCategory", "VariableName", "SampleMedium", "def_unit_id", "timeSupport"]:
+            if isinstance(params[param],list):
+                params[param] = ",".join([str(i) for i in params[param]])
+        headers = {}
+        if self.config["authenticate"]:
+            headers["Authorization"] = "Bearer %s" % self.config["token"]
+        response = requests.get(
+            "%s/obs/variables" % self.config["url"],
+            params = params,
+            headers = headers
+        )
+        print("status_code: %s" % response.status_code)
+        if response.status_code > 299:
+            raise Exception(response.text)
+        json_response = response.json()
+        variables = None
+        if as_DataFrame:
+            variables = pd.DataFrame.from_dict(json_response)
+        else:
+            variables = json_response
+        self.last_result = variables
+        return variables
+
+
+    def getEstaciones(self, fuentes_id : int=None, nombre : str=None, unid : int=None, id : int=None, id_externo : str=None,distrito: str = None, pais: str = None, has_obs : bool=None, real : bool=None, habilitar : bool=None, has_prono : bool=None, rio : str=None, tipo_2 : str=None, geom : str=None, propietario : str=None, automatica : bool=None, ubicacion : str=None, localidad : str=None, tabla : str=None, as_DataFrame : bool=False):
+        params = {"fuentes_id" :fuentes_id, "nombre" : nombre, "unid" : unid, "id" : id, "id_externo" : id_externo,"distrito": distrito, "pais": pais, "has_obs" : has_obs, "real" : real, "habilitar" : habilitar, "has_prono" : has_prono, "rio" : rio, "tipo_2" : tipo_2, "geom" : geom, "propietario" : propietario, "automatica" : automatica, "ubicacion" : ubicacion, "localidad" : localidad, "tabla" : tabla}
+        headers = {}
+        if self.config["authenticate"]:
+            headers["Authorization"] = "Bearer %s" % self.config["token"]
+        response = requests.get(
+            "%s/obs/puntual/estaciones" % (self.config["url"]),
+            params = params,
+            headers = headers
+        )
+        print("status_code: %s" % response.status_code)
+        if response.status_code > 299:
+            raise Exception(response.text)
+        json_response = response.json()
+        estaciones = None
+        if as_DataFrame:
+            estaciones = pd.DataFrame.from_dict(json_response)
+            estaciones["longitude"] = [geom["coordinates"][0] for geom in estaciones["geom"]]
+            estaciones["latitude"] = [geom["coordinates"][1] for geom in estaciones["geom"]]
+            del estaciones["geom"]
+        else:
+            estaciones = json_response
+        self.last_result = estaciones
+        return estaciones
+
+
+if __name__ == "__main__":
+    pass
+    # with open("config.json") as f:
+    #     config = json.load(f)
+
+    # apiLoginParams = config["api"]
+
+    # client = Client(url=apiLoginParams["url"])
+
+    # estaciones = client.getEstaciones(unid=96)
+    # client.writeLastResult("results/estaciones.json")
+    # estaciones = client.getEstaciones(tabla="alturas_prefe",as_DataFrame=True)
+    # client.writeLastResult("results/estaciones.csv")
+    
+    # estaciones = getEstaciones(tabla="ina_delta")
+    # series = getSeries()
+    # series = getSeries(id=29)
+    # series = getSeries(estacion_id=29)
+    # series = getSeries(var_id=58)
+    # series = getSeries(tabla="ina_delta")
+    # series = getSeries(tabla="ina_delta",as_DataFrame=True)
+    # series = getSeries(tabla="ina_delta", no_metadata=True, as_DataFrame=True)
+    # series.columns
+    # series["id"]
+    # set(series["estacion_id"])
+    # set(series["var_id"])
+    # set(series["id_externo"])
+    # set(series["var_nombre"])
+    # series["geom"]
+    # series = getSeries(tabla="ina_delta", no_metadata=True)
+
+    # obs = getObs(29,"2021-01-01","2022-01-01")
